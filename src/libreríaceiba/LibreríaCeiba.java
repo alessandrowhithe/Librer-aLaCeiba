0c449ef165927b0888599bc283933299018a5236
alessandrowhithe <j.alessandromendez.b@gmail.com>
2024-11-11 15:53:07 -0600

    ceiba

---
+++ src/libreríaceiba/LibreríaCeiba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package libreríaceiba;
+
+/**
+ *
+ * @author aless
+ */
+public class LibreríaCeiba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
